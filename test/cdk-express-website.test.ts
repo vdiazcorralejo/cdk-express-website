3085a01f55630b92c1daeb084e6152e8d60d1773
Vicente Díaz-Corralejo <vicenxl@hotmail.com>
2025-11-20 11:17:37 +0100

    Initial commit

---
+++ test/cdk-express-website.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib/core';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkExpressWebsite from '../lib/cdk-express-website-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk-express-website-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkExpressWebsite.CdkExpressWebsiteStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
