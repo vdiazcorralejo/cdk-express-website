9dddd9457f893637189e7d2b854a995ced9f2fe5
Vicente Diaz-Corralejo <vdiazcorralejo@gmail.com>
2025-12-02 15:50:03 +0100

    new SSL certificate and domain personalized

--- lib/cdk-express-website-stack.ts
+++ lib/cdk-express-website-stack.ts
@@ -4,13 +4,14 @@ import * as cloudfront from 'aws-cdk-lib/aws-cloudfront';
 import * as origins from 'aws-cdk-lib/aws-cloudfront-origins';
 import * as s3 from 'aws-cdk-lib/aws-s3';
 import * as s3deploy from 'aws-cdk-lib/aws-s3-deployment';
+import * as acm from 'aws-cdk-lib/aws-certificatemanager';
 
 export class CdkExpressWebsiteStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+  constructor(scope: Construct, id: string, certificate: acm.ICertificate, props?: cdk.StackProps) {
     super(scope, id, props);
 
     // bucket for my website configuration
-    const siteBucket = new s3.Bucket(this, 'SitioWebBucket', {
+    const siteBucket = new s3.Bucket(this, 'WebSiteBucket', {
       publicReadAccess: false,
       removalPolicy: cdk.RemovalPolicy.DESTROY,
       autoDeleteObjects: true,
@@ -21,14 +22,15 @@ export class CdkExpressWebsiteStack extends cdk.Stack {
       destinationBucket: siteBucket,
     });
 
-
-const distribution = new cloudfront.Distribution(this, 'CDNDeMiWeb', {
-  defaultBehavior: {
-    origin: origins.S3BucketOrigin.withOriginAccessControl(siteBucket),
-    viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
-  },
-  defaultRootObject: 'index.html',
-  priceClass: cloudfront.PriceClass.PRICE_CLASS_100,
-});
+    const distribution = new cloudfront.Distribution(this, 'CDNOfMyWeb', {
+      defaultBehavior: {
+      origin: origins.S3BucketOrigin.withOriginAccessControl(siteBucket),
+      viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+      },
+      defaultRootObject: 'index.html',
+      priceClass: cloudfront.PriceClass.PRICE_CLASS_100,
+      domainNames: ['vdiaz-aws.cloud', 'www.vdiaz-aws.cloud'],
+      certificate: certificate,
+    });
   }
 }
---
+++ lib/certificate-stack.ts
@@ -0,0 +1,18 @@
+//TLS certificate
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import * as acm from 'aws-cdk-lib/aws-certificatemanager';
+
+export class CertificateStack extends cdk.Stack {
+  readonly certificate: acm.Certificate;
+
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    this.certificate = new acm.Certificate(this, 'Certificate', {
+      domainName: 'vdiaz-aws.cloud',
+      subjectAlternativeNames: ['www.vdiaz-aws.cloud'],
+      validation: acm.CertificateValidation.fromDns(),
+    });
+  }
+}
