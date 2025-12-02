9fc68c69d753672158fd335ee74f4e01bb883112
Vicente Diaz-Corralejo <vdiazcorralejo@gmail.com>
2025-12-02 15:50:03 +0100

    new SSL certificate and domain personalized

--- bin/cdk-express-website.ts
+++ bin/cdk-express-website.ts
@@ -1,10 +1,21 @@
 #!/usr/bin/env node
-import * as cdk from 'aws-cdk-lib/core';
+import * as cdk from 'aws-cdk-lib';
 import { CdkExpressWebsiteStack } from '../lib/cdk-express-website-stack';
+import { CertificateStack } from '../lib/certificate-stack';
 
 const app = new cdk.App();
-new CdkExpressWebsiteStack(app, 'CdkExpressWebsiteStack', {
- 
-  //env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+const certificateStack = new CertificateStack(app, 'CertificateStack', {
+  env: {
+    account: process.env.CDK_DEFAULT_ACCOUNT,
+    region: 'us-east-1', // Hay que poner esta región
+  },
+  crossRegionReferences: true,
+});
 
-}); 
+new CdkExpressWebsiteStack(app, 'CdkExpressWebsiteStack', certificateStack.certificate, {
+  env: {
+    account: process.env.CDK_DEFAULT_ACCOUNT,
+    region: process.env.CDK_DEFAULT_REGION,
+  },
+  crossRegionReferences: true,
+});
